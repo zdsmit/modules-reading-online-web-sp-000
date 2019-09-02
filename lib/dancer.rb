32b31509c703ba0a06bc208b1641a48d648f5d92
Zach  Smith <zdsmith001@gmail.com>
2019-09-02 16:57:10 +0000

    Done.

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,13 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+
+class Dancer
+  extend MetaDancing
+  include Dance
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,13 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+
+class Kid
+  extend MetaDancing
+  include Dance
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
