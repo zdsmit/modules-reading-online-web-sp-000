25418f43a329d89987984e86e2f5139f9873b27a
Zach  Smith <zdsmith001@gmail.com>
2019-09-03 14:53:46 +0000

    Done.

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,8 +2,8 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid
-  extend MetaDancing
-  include Dance
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
 
   attr_accessor :name
 
